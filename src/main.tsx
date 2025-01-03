25c4469a977dfccf7cedf71dbb966d2734c1b89f
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-03 15:37:26 +0100

    MERGING

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import BilleM7 from './pages/BilleM7/BilleM7.tsx'
 import BilleM5 from './pages/BilleM5/BilleM5.tsx'
 import BilleM3 from './pages/BilleM3/BilleM3.tsx'
 import BilleM2 from './pages/BilleM2/BilleM2.tsx'
-import BilleM2 from './pages/BilleM2/BilleM2.tsx'
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
